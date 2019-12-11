58f039683b2b39ace45c8b6f00ceb3ae0f24f121
subbu subhash <43989764+subbu959@users.noreply.github.com>
2019-12-11 23:16:05 +0530

    Update login_screen.dart

--- login_screen.dart
+++ login_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:gate_pass1/app_screens/home_screen.dart';
 import 'package:gate_pass1/app_screens/signup_screen.dart';
+import 'package:gate_pass1/Animation/FadeAnimation.dart';
 class login_screen extends StatefulWidget{
   @override
   login_screenstate createState() => login_screenstate();
@@ -15,61 +17,103 @@ class login_screen extends StatefulWidget{
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            SizedBox(height: 30.0,),
+            SizedBox(height: 20.0,),
             Column(
                children:<Widget>[
-                 Image.asset('images/logo.png',height: 150,width: 250,),
-                 Text('Gate Pass',style:TextStyle(fontSize: 35,)),
+                 FadeAnimation(0.25,
+                    Image.asset('images/logo.png',height: 150,width: 250,),
+                    ),
+                    FadeAnimation(0.5,                 
+                    Text('Gate Pass',style:TextStyle(fontSize: 25,)),
+                    ),
                  SizedBox(height: 5.0,),
+                 
+                    FadeAnimation(0.5,                 
+                    Text('Login',style:TextStyle(fontSize: 25,)),
+                    ),
                 ],
             ),
             SizedBox(height: 12.0,),
-            TextField(
-              decoration: InputDecoration(
+            FadeAnimation(0.75,   
+              new Container(
+                height: 50.0,
+            child: TextField(
+              decoration:
+              
+               InputDecoration(
                 labelText: 'Email ID',
                 filled: true,
-                border: OutlineInputBorder()
+                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+             
               ),
             ),
+            )
+            ),
             SizedBox(height: 12.0,),
-            TextField(
+            FadeAnimation(1,
+            Container(
+              height:50.0,
+            child:TextField(
               decoration: InputDecoration(
                 labelText: 'Password',
                 filled: true,
+                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
               ),
               obscureText: true,
             ),
-            ButtonBar(
-              children: <Widget>[
-                RaisedButton(
-                  child: Text('Login'),
-                  onPressed: (){},
-                ),
-                ],
+            )
             ),
             ButtonBar(
               children: <Widget>[
-                Text("New User?"),
+                FadeAnimation(1.25,
                 RaisedButton(
-                  child: Text('SignUp'),
+                  child: Text('Login'),
+                  
                   onPressed: (){
-                    tosignup(context);
-                                                   
+                    tohome();
                                       },
+                                      shape: RoundedRectangleBorder(
+                                                                borderRadius: new BorderRadius.circular(20.0)
+                                                              )
+                                    ),
                                     ),
                                     ],
-                                )
+                                ),
+                                ButtonBar(
+                                  children: <Widget>[
+                                    FadeAnimation(1.5,
+                                    Text("New User?"),
+                                    ),
+                                    FadeAnimation(1.75,
+                                    RaisedButton(
+                                      child: Text('SignUp'),
+                                      onPressed: (){
+                                        tosignup(context);
+                                                    },
+                                                    shape: RoundedRectangleBorder(
+                                                                borderRadius: new BorderRadius.circular(20.0)
+                                                              )
+                                                        ),
+                                    ),
+                                    
+                                                        ],
+                                                    )
+                                                  
+                                                    ],
+                                                ),
+                                              ),
+                                            );
+                                          }
+                                        
+                                          void tosignup(BuildContext context) {
+                                          
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>signup_screen()));
                               
-                                ],
-                            ),
-                          ),
-                        );
-                      }
+                                          }
                     
-                      void tosignup(BuildContext context) {
-                      
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>signup_screen()));
-          
+                      void tohome() {
+                         Navigator.push(context, MaterialPageRoute(builder: (context)=>home_screen()));
+                              
                       }
   
 }
