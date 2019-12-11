38841e3e3352f7c25989bce9590a809b528656a6
subbu subhash <43989764+subbu959@users.noreply.github.com>
2019-12-11 23:16:55 +0530

    Update signup_screen.dart

--- signup_screen.dart
+++ signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gate_pass1/Animation/FadeAnimation.dart';
+import 'newuser.dart';
+import 'package:gate_pass1/database_helper.dart';
 class signup_screen extends StatefulWidget{
   @override
   signup_screenstate createState() => signup_screenstate();
@@ -6,6 +9,12 @@ class signup_screen extends StatefulWidget{
   
   class signup_screenstate extends State<signup_screen>{
   @override
+  String _email;
+  String _password;
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+  
+  
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
@@ -13,44 +22,78 @@ class signup_screen extends StatefulWidget{
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            Image.asset('images/logo.png',height: 150,width: 250,),
-                 Text('Gate Pass',style:TextStyle(fontSize: 35,)),
-            TextField(
+ children: <Widget>[
+            SizedBox(height: 20.0,),
+            Column(
+              children: <Widget>[
+            FadeAnimation(0.25,
+            Image.asset('images/logo.png',height: 140,width: 240,),
+            ),
+            FadeAnimation(0.5,
+                 Text('Gate Pass',style:TextStyle(fontSize: 30,)),
+            ),
+            SizedBox(height: 15,),
+            FadeAnimation(0.5,
+                 Text('Sign-Up',style:TextStyle(fontSize: 30,)),
+            ),
+            SizedBox(height: 15,),
+            FadeAnimation(0.75,
+            Container(
+              height: 50.0,
+            child:TextField(
+              controller: myController1,
               decoration: InputDecoration(
                 labelText: 'Email ID',
                 filled: true,
+                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+             
                  ),
             ),
+            )
+            ),
             SizedBox(height: 12.0,),
-            TextField(
+            FadeAnimation(1,
+            Container(
+              height: 50.0,
+            child:TextField(
+              controller: myController2,
               decoration: InputDecoration(
                 labelText: 'Password',
                 filled: true,
+                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+             
               ),
               obscureText: true,
+            ),
+            )
             ),
             SizedBox(height: 12.0,),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Re-Type Password',
-                filled: true,
-              ),
-              obscureText: true,
-            ),
             ButtonBar(
               children: <Widget>[
+                FadeAnimation(1.5,
                 RaisedButton(
                   child: Text('SignUp'),
                   onPressed: (){
-                  },
-                ),
-                ],
-            )
-          
-                ],
-        ),
-      ),
-    );
-  }
+                        
+                                  },
+                                                            shape: RoundedRectangleBorder(
+                                                                                      borderRadius: new BorderRadius.circular(20.0)
+                                                                                    )
+                                          
+                                                          ),
+                                                          )
+                                                          ],
+                                                      )
+                                                    
+                                                          ],
+                                                      )
+                                           ]
+                                                
+                                                ),
+                                              )
+                                              );
+                                            }
+                        
+                                        
+                  
 }
