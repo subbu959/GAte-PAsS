bbeb7cf41e059c3be32e32e2f0a2479b80e4c32e
subbu subhash <43989764+subbu959@users.noreply.github.com>
2019-12-11 23:17:25 +0530

    Update flash_screen.dart

--- flash_screen.dart
+++ flash_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:gate_pass1/app_screens/login_screen.dart';
+import 'package:gate_pass1/Animation/FadeAnimation.dart';
+
 class flash_screen extends StatefulWidget{
   @override
 _flash_screenstate createState()=> _flash_screenstate();
@@ -9,7 +11,7 @@ class _flash_screenstate extends State<flash_screen>{
   @override
   void initState(){
     super.initState();
-    Timer(Duration(milliseconds: 7000), 
+    Timer(Duration(milliseconds: 3000), 
     () =>Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=>login_screen())));
   }
   Widget build(BuildContext context) => Scaffold(
@@ -19,8 +21,13 @@ class _flash_screenstate extends State<flash_screen>{
        child:Column(
            children:<Widget>[
              SizedBox(height: 90.0,),
+             
+              FadeAnimation(1,
          Image.asset('images/logo.png',height: 250,width: 300,),
+              ),
+          FadeAnimation(1.25,    
          Text('Gate Pass',style: TextStyle(fontSize: 45),),
+          )
            ]
          )
     ),
